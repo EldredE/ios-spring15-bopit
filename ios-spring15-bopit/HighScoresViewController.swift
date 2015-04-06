18c75f968e694b3a9851a8fa1fcb29ccd10f80a4
Ethan Eldred <ethan_eldred@yahoo.com>
2015-04-05 19:18:27 -0500

    Added the Main Screen as well as the High Scores, Nothing done with high scores yet.

---
+++ ios-spring15-bopit/HighScoresViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HighScoresViewController.swift
+//  ios-spring15-bopit
+//
+//  Created by Ethan Eldred on 4/5/15.
+//  Copyright (c) 2015 Ethan Eldred. All rights reserved.
+//
+
+import UIKit
+
+class HighScoresViewController: UIViewController  {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
